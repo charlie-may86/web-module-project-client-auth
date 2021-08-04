f73e48faee7ab58c5fb8d365931946b7e9938915
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-08-04 07:26:20 -0500

    login form built

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <h1>This is my app know - I am the captain</h1>
+      <Login />
     </div>
   );
 }
---
+++ friends/src/components/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+// const credentials = {
+//   username: "",
+//   password: "",
+// };
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  console.log(credentials);
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
